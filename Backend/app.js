3bf86ca48af03785770263389ce61228b31214d3
Arindam Sarkar <43996902+sarkar10425@users.noreply.github.com>
2020-09-23 13:02:23 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -2,7 +2,7 @@ const express=require('express')
 const app = express()
 const Slots = require('./api/models/slots')
 const slots = new Slots()
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
